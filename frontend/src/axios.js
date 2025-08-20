6484d0f6c55b916b94c915a9674d83865770bfe3
AadityaSharma01 <144672180+AadityaSharma01@users.noreply.github.com>
2025-08-20 14:25:26 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://biz-nest-gamma.vercel.apphttps://biz-nest-gamma.vercel.app',
+    baseURL: 'https://biz-nest-gamma.vercel.app',
     withCredentials: true
 })
 

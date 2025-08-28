c2fc1884b698f5834c2136966705fa7616fc81d1
AadityaSharma01 <144672180+AadityaSharma01@users.noreply.github.com>
2025-08-28 14:50:40 +0530

    Update aires.controller.js

--- backend/controllers/aires.controller.js
+++ backend/controllers/aires.controller.js
@@ -2,7 +2,6 @@ import cohereCall from "../services/aiComponent.js";
 
 export const postAires = async (req, res) => {
     const { body } = req
-    console.log("from postAires -->", body, "<-- from postAires")
     const result = await cohereCall(JSON.stringify(body))
     res.json({ result })
 };
